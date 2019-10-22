49b785a7b4a7cf8bc48a80a2ad127eeb9c8aa243
Orquidea <orquidea_238@hotmail.fr>
2019-10-22 11:04:25 +0200

    first commit

---
+++ userQueries.js
@@ -0,0 +1,6 @@
+// this file catch the users information and show it in the console
+const lastname = prompt("Quel est votre nom?");
+const firstname = prompt("Quel est votre prenom?");
+const age = prompt("Quel est votre age?");
+
+console.log("Bonjour " + firstname + " " + lastname + "." + " Vous avez " + age + " ans.");
